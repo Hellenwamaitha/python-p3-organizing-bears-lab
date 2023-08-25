65f86d12ca181bcd341e79752e821362a127474f
hellenwamaitha <hellenwama254@gmail.com>
2023-08-25 17:02:17 +0300

    updated lab

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+    ('Mr. Chocolate', 5, 'M', 'Yellow', 'Friendly', 1),
+    ('Rowdy', 6, 'M', 'Red', 'Playful', 1),
+    ('Tabitha', 7, 'F', 'Purple', 'Calm', 1),
+    ('Sergeant Brown', 10, 'M', 'Green', 'Serious', 0),
+    ('Melissa', 12, 'F', 'White', 'Energetic', 1),
+    ('Grinch', 14, 'M', 'Grey', 'Grumpy', 0),
+    ('Wendy', 9, 'F', 'Blue', 'Cheerful', 1),
+    ('unnamed', 5, 'F', 'Black', 'Unknown', NULL);
+
